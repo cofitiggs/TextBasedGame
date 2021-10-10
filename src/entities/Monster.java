8a5e557223c0265d3fbb8da7f676b337531390c8
Cofi Tiggs <cofi.tiggs@gmail.com>
2021-10-09 21:53:23 -0400

    Added lombok annotations to reduce boilerplate code

--- src/entities/Monster.java
+++ src/entities/Monster.java
@@ -1,50 +1,11 @@
 package entities;
 
+import lombok.Data;
+
+@Data
 public class Monster {
     private int defense;
     private int attackSpeed;
     private int damage;
     private int hitPoints;
-
-    public int getDefense() {
-        return this.defense;
-    }
-
-    public void setDefense(int defense) {
-        this.defense = defense;
-    }
-
-    public int getAttackSpeed() {
-        return this.attackSpeed;
-    }
-
-    public void setAttackSpeed(int attackSpeed) {
-        this.attackSpeed = attackSpeed;
-    }
-
-    public int getDamage() {
-        return this.damage;
-    }
-
-    public void setDamage(int damage) {
-        this.damage = damage;
-    }
-
-    public int getHitPoints() {
-        return this.hitPoints;
-    }
-
-    public void setHitPoints(int hitPoints) {
-        this.hitPoints = hitPoints;
-    }
-
-    @Override
-    public String toString() {
-        return "Monster{" +
-                "defense=" + defense +
-                ", attackSpeed=" + attackSpeed +
-                ", damage=" + damage +
-                ", hitPoints=" + hitPoints +
-                '}';
-    }
 }
