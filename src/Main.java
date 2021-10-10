a045cabf0865a507126504e0b053ff55fcd74a70
Cofi Tiggs <cofi.tiggs@gmail.com>
2021-10-09 20:07:48 -0400

    Simulation of simple fight

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,43 @@
-public class Main {
+import entities.Monster;
+import entities.Player;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main {
+    public static void main(String[] args){
+        Player testPlayer = new Player();
+        testPlayer.setHitPoints(20);
+        testPlayer.setDamage(2);
+
+        Monster testMonster = new Monster();
+        testMonster.setHitPoints(10);
+        testMonster.setDamage(1);
+
+        System.out.println("You have entered a fight with a monster! Get ready!");
+
+        int playerHP = testPlayer.getHitPoints();
+        int playerDamage = testPlayer.getDamage();
+        int playerHpRemaining = playerHP;
+
+        int monsterHP = testMonster.getHitPoints();
+        int monsterDamage = testMonster.getDamage();
+        int monsterHpRemaining = monsterHP;
+
+        while (playerHpRemaining > 0 && monsterHpRemaining > 0) {
+            System.out.println("You inflict " + playerDamage + " damage on the monster.");
+            monsterHpRemaining -= playerDamage;
+            testMonster.setHitPoints(monsterHpRemaining);
+            System.out.println("The monster has " + testMonster.getHitPoints() + " hitpoints remaining.");
+            if (monsterHpRemaining <= 0){
+                System.out.println("Congratulations! You have defeated the monster!");
+                break;
+            }
+            System.out.println("The monster inflicts " + monsterDamage + "damage on you.");
+            playerHpRemaining -= monsterDamage;
+            testPlayer.setHitPoints(playerHpRemaining);
+            System.out.println("You have " + testPlayer.getHitPoints() + " hitpoints remaining.");
+            if (playerHpRemaining <= 0){
+                System.out.println("Sorry, but you have been defeated by the monster.");
+                break;
+            }
+        }
     }
 }
